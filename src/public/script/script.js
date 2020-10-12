23744ff57bbad721ae5d9d19440209458eaf60ba
GermanCode <w.aguirre@udla.edu.co>
2020-10-12 13:54:05 -0500

    completo labores

---
+++ src/public/script/script.js
@@ -0,0 +1,17 @@
+function llenarResp() {
+    var resp = document.getElementById("selectResponsable");
+    var idResp = resp.value;
+    var nombResp = resp.options[resp.selectedIndex].innerText;
+    var hr= document.getElementById("hiddenResp")
+    hr.value = idResp;
+    alert(idResp);
+    };
+
+    function llenarLab() {
+    var lab = document.getElementById("selectLabor");
+    var idLab = lab.value;
+    var descLab = lab.options[lab.selectedIndex].innerText;
+    var hl = document.getElementById("hiddenLab");
+    hl.value = idLab;
+
+    };
